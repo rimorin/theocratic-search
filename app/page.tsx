53cc47b5e79015de8d722656745a0256a84343c4
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2023-07-07 16:45:08 +0800

    added header information

--- app/page.tsx
+++ app/page.tsx
@@ -29,92 +29,137 @@ export default function Home() {
   };
 
   return (
-    <div className="mx-auto max-w-screen-xl min-h-screen px-4 py-16 sm:px-6 lg:px-8">
-      <form className="mx-auto mb-0 mt-8 space-y-4">
-        <div className="relative">
-          <label htmlFor="Search" className="sr-only">
-            {" "}
-            Search{" "}
-          </label>
+    <>
+      <header>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <div className="text-center sm:text-left">
+              <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                Theocratic Search
+              </h1>
 
-          <input
-            type="text"
-            id="Search"
-            placeholder="Ask me a question ..."
-            className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={handleEnterKey}
-            disabled={isSearching}
-          />
-
-          <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
-            <button
-              type="button"
-              className="text-gray-600 hover:text-gray-700"
-              onClick={handleClick}
-              disabled={isSearching}
-            >
-              <span className="sr-only">Search</span>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-4 w-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </button>
-          </span>
-        </div>
-        <div className="items-center relative">
-          <label className="sr-only" htmlFor="message">
-            Message
-          </label>
-
-          <textarea
-            className={`w-full rounded-lg border-gray-200 p-3 ${
-              isSearching && "opacity-20 dark:bg-gray-200"
-            }`}
-            disabled
-            rows={10}
-            id="message"
-            value={searchResults}
-            style={{ resize: "none" }}
-          ></textarea>
-          {isSearching && (
-            <div
-              role="status"
-              className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
-            >
-              <svg
-                aria-hidden="true"
-                className="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span className="sr-only">Loading...</span>
+              <p className="mt-1.5 text-sm text-gray-500">
+                A new way to search for answers 🔍
+              </p>
             </div>
-          )}
+            <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+              <button
+                className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
+                type="button"
+                onClick={() =>
+                  window.open(
+                    "https://github.com/rimorin/theocratic-search",
+                    "_blank"
+                  )
+                }
+              >
+                <span className="text-sm font-medium">Learn More</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
-      </form>
-    </div>
+      </header>
+      <div className="mx-auto max-w-screen-xl min-h-screen px-4 sm:px-6 lg:px-8">
+        <form className="mx-auto mb-0 mt-8 space-y-4">
+          <div className="relative">
+            <label htmlFor="Search" className="sr-only">
+              Search
+            </label>
+
+            <input
+              type="text"
+              id="Search"
+              placeholder="Ask me a question ..."
+              className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={handleEnterKey}
+              disabled={isSearching}
+            />
+
+            <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+              <button
+                type="button"
+                className="text-gray-600 hover:text-gray-700"
+                onClick={handleClick}
+                disabled={isSearching}
+              >
+                <span className="sr-only">Search</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-4 w-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </button>
+            </span>
+          </div>
+          <div className="items-center relative">
+            <label className="sr-only" htmlFor="message">
+              Message
+            </label>
+
+            <textarea
+              className={`w-full rounded-lg border-gray-200 p-3 ${
+                isSearching && "opacity-20 dark:bg-gray-200"
+              }`}
+              disabled
+              rows={10}
+              id="message"
+              value={searchResults}
+              style={{ resize: "none" }}
+            ></textarea>
+            {isSearching && (
+              <div
+                role="status"
+                className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
