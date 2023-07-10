6e7732e29886a70d43fc0cd8c4ea47b2a3118c98
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2023-07-10 21:55:07 +0800

    Improved prompting

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,31 +1,29 @@
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeClient } from "@pinecone-database/pinecone";
-import {
-  ConversationalRetrievalQAChain,
-  RetrievalQAChain,
-} from "langchain/chains";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain";
+import { PromptTemplate } from "langchain/prompts";
 
 export const runtime = "edge";
 
 const { PINECONE_API_KEY, PINECONE_ENVIRONMENT, PINECONE_INDEX } = process.env;
-const PARAM_NAME = "query";
 const OpenAiLLM = new OpenAI({ temperature: 0 });
 
-const TEMPLATE = `
+const COMMON_TEMPLATE = `If you don't know the answer, just say that you don't know.
+Do not use outside sources.
+Include scripture references if applicable.
+Use the NWT translation when quoting scriptures.`;
+
+const PROMPT_TEMPLATE = `
 Your task is to answer the following question:
 
 {question}
 
-If you don't know the answer, just say that you don't know.
-Do not use outside sources.
-Include scripture references if applicable.
-Use the NWT translation when quoting scriptures.
+${COMMON_TEMPLATE}
 `;
 const LLMPrompt = new PromptTemplate({
-  template: TEMPLATE,
+  template: PROMPT_TEMPLATE,
   inputVariables: ["question"],
 });
 
@@ -39,62 +37,8 @@ const initialisePinconeIndex = async () => {
   return client.Index(PINECONE_INDEX || "");
 };
 
-export async function GET(request: Request) {
-  // check empty request
-  if (!request) {
-    return new Response(
-      JSON.stringify({
-        error: "Empty request",
-      })
-    );
-  }
-  // check empty query parameter
-  if (!request.url) {
-    return new Response(
-      JSON.stringify({
-        error: "Empty query parameter",
-      })
-    );
-  }
-  // check missing environment variables
-  if (!PINECONE_API_KEY || !PINECONE_ENVIRONMENT || !PINECONE_INDEX) {
-    return new Response(
-      JSON.stringify({
-        error: "Missing environment variables",
-      })
-    );
-  }
-
-  const params = new URL(request.url).searchParams;
-  const query = params.get(PARAM_NAME);
-  // check empty query parameter
-  if (!query) {
-    return new Response(
-      JSON.stringify({
-        error: "Empty query parameter",
-      })
-    );
-  }
-
-  const question = await LLMPrompt.format({ question: query });
-  const pineconeIndex = await initialisePinconeIndex();
-  const vectorStore = await PineconeStore.fromExistingIndex(
-    new OpenAIEmbeddings(),
-    { pineconeIndex }
-  );
-  const chain = RetrievalQAChain.fromLLM(OpenAiLLM, vectorStore.asRetriever());
-
-  const response = await chain.call({
-    query: question,
-  });
-  return new Response(JSON.stringify(response));
-}
-
 export async function POST(request: Request) {
-  // get question string and chats list from request body
   const { question, chats } = await request.json();
-  console.log(question);
-  console.log(chats);
 
   // check empty question
   if (!question) {
@@ -113,7 +57,6 @@ export async function POST(request: Request) {
       })
     );
   }
-
   const prompt = await LLMPrompt.format({ question: question });
   const pineconeIndex = await initialisePinconeIndex();
   const vectorStore = await PineconeStore.fromExistingIndex(
@@ -127,8 +70,14 @@ export async function POST(request: Request) {
   );
 
   const chatsString = chats
-    .filter((chat: any) => chat.type === "answer")
-    .map((chat: any) => chat.message)
+    .map((chat: any) => {
+      // if chat is a question, return the question
+      if (chat.type === "question") {
+        return `Human: ${chat.message}`;
+      }
+      // if chat is an answer, return the question and answer
+      return `AI: ${chat.message}`;
+    })
     .join("\n");
 
   const response = await chain.call({
