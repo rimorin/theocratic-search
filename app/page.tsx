d83e967aa71cd92761ca41103a91634f0b7b55a3
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2023-07-10 22:09:09 +0800

    Fixed width for conversation div

--- app/page.tsx
+++ app/page.tsx
@@ -202,7 +202,7 @@ export default function Home() {
         </div>
         <div
           id="conversation"
-          className="overflow-auto max-w-4xl px-4 pb-10 sm:px-6 lg:px-8 lg:py-14 h-full
+          className="overflow-auto px-4 pb-10 sm:px-6 lg:px-8 lg:py-14 h-full
           "
         >
           {chats && (
